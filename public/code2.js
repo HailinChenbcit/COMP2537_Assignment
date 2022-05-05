666cc4acfa111952d776d56239d2cfc35b64066f
hailin chen <hchen256@my.bcit.ca>
2022-05-05 00:04:37 -0700

    Added pagination for search by type

--- public/code2.js
+++ public/code2.js
@@ -1,29 +1,34 @@
 poke_id = null
 poke_name = null
 poke_type = null
+page_number = null
 g_data = null
 
-function pagination(g_data){
-    console.log(g_data)
-    
-}
 
-async function process_type(data) {
-    g_data = data
+async function paginate(page_number) {
+    let total_pages = Math.ceil(g_data.pokemon.length / 9)
+    // display page buttons
+    var html = ""
+    for (cur_page = 0; cur_page < total_pages; cur_page++) {
+        html += `<span><button id="${cur_page + 1}">${cur_page + 1}</button></span>`
+        $("#page_numbers").html(html)
+    }
+
+    html = ""
+
     pokemons = []
     pokemon_ids = []
-    for (i = 0; i < data["pokemon"].length; i++) {
-        pokemons.push(data["pokemon"][i]["pokemon"]["name"])
-        id = data["pokemon"][i]["pokemon"]["url"]
+    for (i = 0; i < g_data["pokemon"].length; i++) {
+        pokemons.push(g_data["pokemon"][i]["pokemon"]["name"])
+        id = g_data["pokemon"][i]["pokemon"]["url"]
         pokemon_ids.push(id.substr(34).slice(0, -1))
     }
 
     result = ""
 
-    for (j = 0; j < pokemons.length; j++) {
-        poke_name = pokemons[j]
-        poke_id = pokemon_ids[j]
-        one_column = parseInt(pokemons.length / 3)
+    for (i = (page_number - 1) * 9; i < page_number * 9; i++) {
+        poke_name = pokemons[i]
+        poke_id = pokemon_ids[i]
 
         await $.ajax(
             {
@@ -31,23 +36,28 @@ async function process_type(data) {
                 "type": 'GET',
                 "success": function process() {
                     result += `<div class="image_container"> <a href='profile/${poke_id}' id= ${poke_id}>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke_id}.png">
-                    </img>
-                    </a>
-                    <div class='card_header'>
-                    <div class='poke__number'>
-                    #${poke_id}
-                    </div>
-                    </div>
-                    <div>${poke_name}</div>
-                    </div>`
+                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke_id}.png">
+                            </img>
+                            </a>
+                            <div class='card_header'>
+                            <div class='poke__number'>
+                            #${poke_id}
+                            </div>
+                            </div>
+                            <div>${poke_name}</div>
+                            </div>`
                 }
             }
         )
-
     }
-
     $("main").html(result)
+
+}
+
+
+function process_type(data) {
+    g_data = data
+    paginate(1)
 }
 
 
@@ -88,7 +98,6 @@ async function process_region(data) {
         )
 
     }
-
     $("main").html(result)
 }
 
@@ -119,12 +128,15 @@ function display_by_region() {
 
 $(document).ready(function () {
     display_by_type()
+
     $("#poke_type").change(() => {
         display_by_type()
     })
+
     $("#poke_region").change(() => {
         display_by_region()
     })
+
     $("#search").click(() => {
         poke_name = $("#poke_name").val()
         $.ajax(
@@ -151,5 +163,25 @@ $(document).ready(function () {
             }
         )
         $("main").html(result)
+    })
+
+    // display prev/next button and select page number
+    $("#page_numbers").on("click", "button", (function () {
+        page_number = this.id
+        paginate(page_number)
+    }))
+
+    // Prev/ Next button
+    $("#prev").on("click", () => {
+        if (page_number > 1) {
+            page_number--;
+            paginate(page_number)
+        }
+    })
+    $("#next").on("click", () => {
+        if (page_number * 9 < g_data.results.length) {
+            page_number++;
+            paginate(page_number)
+        }
     })
 })
