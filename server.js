3a0976ccadaf440665941c2ec3bcfbb414bc00c9
hailin chen <hchen256@my.bcit.ca>
2022-05-04 19:26:50 -0700

    Updated profile page

--- server.js
+++ server.js
@@ -18,9 +18,21 @@ app.get('/profile/:id', function (req, res) {
         https_res.on("end", function () {
             // console.log(JSON.parse(data))
             data = JSON.parse(data)
-
+            console.log(data)
             obj_hp = data.stats.filter((obj)=>{
                 return obj.stat.name == 'hp'
+            }).map((obj)=>{
+                return obj.base_stat
+            })
+            
+            obj_atk = data.stats.filter((obj)=>{
+                return obj.stat.name == 'attack'
+            }).map((obj)=>{
+                return obj.base_stat
+            })
+
+            obj_defense = data.stats.filter((obj)=>{
+                return obj.stat.name == 'defense'
             }).map((obj)=>{
                 return obj.base_stat
             })
@@ -28,7 +40,11 @@ app.get('/profile/:id', function (req, res) {
             res.render('profile.ejs', {
                 "id": req.params.id,
                 'name': data.name,
-                "hp": obj_hp[0]
+                "hp": obj_hp[0],
+                "weight": data.weight,
+                "height": data.height,
+                "attack": obj_atk[0],
+                "defense": obj_defense[0],
             })
         })
     })
