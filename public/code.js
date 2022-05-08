7b827ba5af3a9fdacc744e6b412b1f09e567725f
hailin chen <hchen256@my.bcit.ca>
2022-05-07 23:29:37 -0700

    Updated colors for different types

--- public/code.js
+++ public/code.js
@@ -3,24 +3,24 @@ poke_name = null
 pokemons = []
 
 const colors = {
-    fire: '#FDDFDF',
-    grass: '#DEFDE0', 
-    electric: '#FCF7DE', 
-    water: '#DEF3FD', 
-    ground: '#f4e7da', 
-    rock: '#d5d5d4', 
-    fairy: '#fceaff', 
-    poison: '#98d7a5', 
-    bug: '#f8d5a3', 
-    dragon: '#97b3e6', 
-    psychic: '#eceda1', 
-    flying: '#F5F5F5', 
-    fighting: '#E6E0D4', 
-    normal: '#F5F5F5',
-    ice:"#37f0e8",
-    dark:"#370000",
-    steel:"226945",
-    shadow:"#e6edee",
+    fire: ['#FDDFDF', '#fccccc'],
+    grass: ['#aeeab2', '#ccfcd0'],
+    electric: ['#FCF7DE', '#faefb9'],
+    water: ['#DEF3FD', '#bbe6fa'],
+    ground: ['#f4e7da', '#fad8b6'],
+    rock: ['#d5d5d4', '#a6a6a2'],
+    fairy: ['#fceaff', '#edbff5'],
+    poison: ['#77f37f', '#3bd15a'],
+    bug: ['#f8d5a3', '#fcba5b'],
+    dragon: ['#97b3e6', '#6384c2'],
+    psychic: ['#eceda1', '#f0f283'],
+    flying: ['#F5F5F5', '#f5e9e9'],
+    fighting: ['#ff8c8c', '#bfb6a4'],
+    normal: ['#f5ffcb', '#fafae3'],
+    ice: ["#37f0e8", '#1ae8df'],
+    dark: ["#370000", '#5c0101'],
+    steel: ["#226945", '#0ad670'],
+    shadow: ["#e6edee", '#93a1a3'],
 }
 
 async function display_all_picture() {
@@ -38,7 +38,7 @@ async function display_all_picture() {
                         obj_colors = Object.keys(colors).filter(key => allowed.includes(key))
                         color_code = colors[obj_colors]
                         poke_name = data.name
-                        result += `<div class="image_container" style="background-color:${color_code};"> <a href='profile/${poke_id}' id= ${poke_id}>
+                        result += `<div class="image_container" style="background:linear-gradient(45deg, ${color_code[0]}, ${color_code[1]});"> <a href='profile/${poke_id}' id= ${poke_id}>
                         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke_id}.png">
                         </img>
                         </a>
